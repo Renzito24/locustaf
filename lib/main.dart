30febe2efea2beaa40b116cc8662403a1464d4da
Renzo <renzo.yamil.93@gmail.com>
2026-09-22 23:04:52 -0300

    feat(app-check): activa App Check en cliente Flutter (modo monitoreo, sin enforcement)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:flutter/foundation.dart' show kReleaseMode;
 
 import 'firebase_options.dart';
 import 'app/app.dart';
@@ -10,6 +12,16 @@ void main() async {
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await FirebaseAppCheck.instance.activate(
+    providerAndroid: kReleaseMode
+        ? AndroidPlayIntegrityProvider()
+        : AndroidDebugProvider(),
+    providerApple: kReleaseMode
+        ? AppleAppAttestWithDeviceCheckFallbackProvider()
+        : AppleDebugProvider(),
+    providerWeb: ReCaptchaV3Provider('TU_RECAPTCHA_SITE_KEY_AQUI'),
   );
 
   runApp(
